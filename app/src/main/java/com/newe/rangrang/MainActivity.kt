b39a11349716defadf153d9720948c77d9d9f923
JaylenHsieh <zjjxxiejunliang@gmail.com>
2018-04-19 23:07:40 +0800

    完成了底栏，四个Fragment的创建

---
+++ app/src/main/java/com/newe/rangrang/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.newe.rangrang
+
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomBar.setOnTabSelectListener{tabId ->
+            when(tabId){
+                R.id.tab_flash -> replaceFragment(FlashFragment())
+                R.id.tab_screen -> replaceFragment(ScreenFragment())
+                R.id.tab_history -> replaceFragment(HistoryFragment())
+                R.id.tab_profile -> replaceFragment(ScreenFragment())
+                else -> Toast.makeText(this,"出错啦",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container,fragment)
+                .commit()
+    }
+}
