eba91b51ea5561521f8b395196ad85e9ba44d3e4
JaylenHsieh <zjjxxiejunliang@gmail.com>
2018-04-22 21:43:07 +0800

    闪光灯模式完成了打开和关闭闪光灯，屏幕模式完成了图标的更改，待添加声音文件

--- app/src/main/java/com/newe/rangrang/FlashFragment.java
+++ app/src/main/java/com/newe/rangrang/FlashFragment.java
@@ -1,12 +1,20 @@
 package com.newe.rangrang;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Toast;
 
 
 /**
@@ -14,6 +22,12 @@ import android.view.ViewGroup;
  */
 public class FlashFragment extends Fragment {
 
+    private CameraManager mCameraManager;
+    private Context mContext;
+    private FloatingActionButton mFab;
+    private boolean isFlashOn = false;
+
+    private final int PERMISSION_REQUEST_CAMERA = 1;
 
     public FlashFragment() {
         // Required empty public constructor
@@ -24,7 +38,74 @@ public class FlashFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_flash, container, false);
+        View view = inflater.inflate(R.layout.fragment_flash, container, false);
+        mFab = view.findViewById(R.id.fab_flash);
+        return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mContext == null) {
+            mContext = getContext();
+        }
+        if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+        }else {
+            openTorch(true);
+        }
+        mCameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isFlashOn) {
+                    // 点击后,如果闪光灯是打开的，关闭闪光灯，图标显示为手电关闭，提示用户已关闭闪光灯
+                    mFab.setImageDrawable(getResources().getDrawable(R.mipmap.ic_flash_off));
+                    Toast.makeText(getContext(), "已关闭闪光灯", Toast.LENGTH_SHORT).show();
+                    isFlashOn = false;
+                    openTorch(false);
+                } else {
+                    mFab.setImageDrawable(getResources().getDrawable(R.mipmap.ic_flash_on));
+                    Toast.makeText(getContext(), "已打开闪光灯", Toast.LENGTH_SHORT).show();
+                    isFlashOn = true;
+                    openTorch(true);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        if (mCameraManager != null) {
+            try {
+                mCameraManager.setTorchMode("0", false);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void openTorch(boolean enable) {
+        if (mCameraManager != null) {
+            try {
+                mCameraManager.setTorchMode("0", enable);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CAMERA){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                openTorch(true);
+            }else{
+                Toast.makeText(mContext, "权限被拒绝，我们需要你的相机权限来打开闪光灯", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
--- app/src/main/java/com/newe/rangrang/ScreenFragment.java
+++ app/src/main/java/com/newe/rangrang/ScreenFragment.java
@@ -2,17 +2,26 @@ package com.newe.rangrang;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ScreenFragment extends Fragment {
+
+    private FloatingActionButton mFab;
+
+    /**
+     * 默认关闭提示音
+     */
+    private boolean isTrumpetOn = false;
 
 
     public ScreenFragment() {
@@ -24,7 +33,28 @@ public class ScreenFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_screen, container, false);
+        View view = inflater.inflate(R.layout.fragment_screen, container, false);
+        mFab = view.findViewById(R.id.fab_sound);
+        return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isTrumpetOn){
+                    // 如果扬声器是打开的，点击后关闭提示音，图标显示为喇叭关闭，提示用户已关闭提示音
+                    mFab.setImageDrawable(getResources().getDrawable(R.mipmap.ic_volume_off_white_24dp));
+                    Toast.makeText(getContext(), "已关闭提示音", Toast.LENGTH_SHORT).show();
+                    isTrumpetOn  = false;
+                }else{
+                    mFab.setImageDrawable(getResources().getDrawable(R.mipmap.ic_volume_up_white_24dp));
+                    Toast.makeText(getContext(), "已打开提示音", Toast.LENGTH_SHORT).show();
+                    isTrumpetOn = true;
+                }
+            }
+        });
+    }
 }
